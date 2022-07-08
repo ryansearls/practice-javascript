8d5673e6d159fb76d69a0cd1d1b41af3ad6570cc
Ryan Searls <Ryan.searls@gmail.com>
2022-07-08 11:05:00 -0600

    Interpolate Name

--- skills-sheet/strings_practice.js
+++ skills-sheet/strings_practice.js
@@ -1,8 +1,16 @@
 // # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-var firstName = "Ryan";
-var lastName = "Searls";
-console.log(firstName + " " + lastName)
+// var firstName = "Ryan";
+// var lastName = "Searls";
+// console.log(firstName + " " + lastName)
 
 //------------------------------------------------------------------------
+
+// 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the ${} operator).
+
+var firstName = "Ryan";
+var lastName = "Searls";
+console.log(`${firstName} ${lastName}`);
+
+//----------------------------------------------------------------------------
 
