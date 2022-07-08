a3ba8cee6685d48242b4daaaefccdb5082f290dd
Ryan Searls <Ryan.searls@gmail.com>
2022-07-07 18:33:11 -0600

    Print 1/0/-1

--- skills-sheet/conditionals2.js
+++ skills-sheet/conditionals2.js
@@ -1,11 +1,24 @@
 // 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-var number = 10;
-if (number === 10) {
-  console.log(0);
-} else {
-  console.log(-1);
-}
+// var number = 10;
+// if (number === 10) {
+//   console.log(0);
+// } else {
+//   console.log(-1);
+// }
 
 //--------------------------------------------------------------------
+
+// 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+var number = 3;
+if (number < 10) {
+  console.log(-1);
+} else if (number > 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
+
+//-----------------------------------------------------------------------
 
