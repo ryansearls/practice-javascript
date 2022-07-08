2e6d0eeefc00596f5c28852b57ba7a22654dff46
Ryan Searls <Ryan.searls@gmail.com>
2022-07-08 10:51:30 -0600

    First and Last Name

---
+++ skills-sheet/strings_practice.js
@@ -0,0 +1,8 @@
+// # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+var firstName = "Ryan";
+var lastName = "Searls";
+console.log(firstName + " " + lastName)
+
+//------------------------------------------------------------------------
+
