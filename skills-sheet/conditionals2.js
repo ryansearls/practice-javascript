8c0aded404862484eea1db9f6bb30c2aa8b14042
Ryan Searls <Ryan.searls@gmail.com>
2022-07-07 18:34:59 -0600

    Both Numbers Less Than 10

--- skills-sheet/conditionals2.js
+++ skills-sheet/conditionals2.js
@@ -11,14 +11,27 @@
 
 // 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-var number = 3;
-if (number < 10) {
-  console.log(-1);
-} else if (number > 10) {
+// var number = 3;
+// if (number < 10) {
+//   console.log(-1);
+// } else if (number > 10) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+//-----------------------------------------------------------------------
+
+// 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+var number1 = 4;
+var number2 = 9;
+if (number1 < 10 && number2 < 10) {
   console.log(1);
 } else {
   console.log(0);
 }
 
-//-----------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
 
