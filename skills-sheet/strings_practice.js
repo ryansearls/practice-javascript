654333be917fe1ee28dbfa691b083634dfcabe28
Ryan Searls <Ryan.searls@gmail.com>
2022-07-08 11:18:34 -0600

    Marco Polo

--- skills-sheet/strings_practice.js
+++ skills-sheet/strings_practice.js
@@ -8,9 +8,19 @@
 
 // 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the ${} operator).
 
-var firstName = "Ryan";
-var lastName = "Searls";
-console.log(`${firstName} ${lastName}`);
+// var firstName = "Ryan";
+// var lastName = "Searls";
+// console.log(`${firstName} ${lastName}`);
 
 //----------------------------------------------------------------------------
+
+// 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// var word = window.prompt("Enter a word:");
+// if (word === "marco") {
+//   console.log("polo");
+// }
+
+//-----------------------------------------------------------------------------
+
 
