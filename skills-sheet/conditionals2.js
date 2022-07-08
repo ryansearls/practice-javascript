a4cf557b462d7f33ae1ce9a8596d1a95f5b25fe2
Ryan Searls <Ryan.searls@gmail.com>
2022-07-07 18:54:31 -0600

    One or the Other

--- skills-sheet/conditionals2.js
+++ skills-sheet/conditionals2.js
@@ -47,15 +47,27 @@
 
 // 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
-var number = 11;
-if (number < 10) {
-  console.long(9);
-} else if (number < 20) {
-  console.log(19);
-} else if (number < 30) {
-  console.log(29);
+// var number = 11;
+// if (number < 10) {
+//   console.long(9);
+// } else if (number < 20) {
+//   console.log(19);
+// } else if (number < 30) {
+//   console.log(29);
+// } else {
+//   console.log(-1);
+// }
+
+//----------------------------------------------------------------------------
+
+// 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+var number1 = 50
+var number2 = 105
+if (number1 > 10 || number2 > 10) {
+  console.log(100);
 } else {
-  console.log(-1);
+  console.log(-100);
 }
 
 //----------------------------------------------------------------------------
