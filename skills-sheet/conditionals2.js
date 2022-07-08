daa67783bf69accc88bcb87bfca5ad9cf78a950f
Ryan Searls <Ryan.searls@gmail.com>
2022-07-07 18:47:44 -0600

    Number less than 30

--- skills-sheet/conditionals2.js
+++ skills-sheet/conditionals2.js
@@ -36,12 +36,29 @@
 
 // 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-var power = 9001;
-if (power > 9000) {
-  console.log(1);
+// var power = 9001;
+// if (power > 9000) {
+//   console.log(1);
+// } else {
+//   console.log(-1);
+// }
+
+//--------------------------------------------------------------------------
+
+// 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+var number = 11;
+if (number < 10) {
+  console.long(9);
+} else if (number < 20) {
+  console.log(19);
+} else if (number < 30) {
+  console.log(29);
 } else {
   console.log(-1);
 }
 
-//--------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+
 
