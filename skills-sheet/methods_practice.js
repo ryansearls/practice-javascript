a7056f0acd248e1bf76290687b85107d061208e7
Ryan Searls <Ryan.searls@gmail.com>
2022-07-09 15:47:40 -0600

    Number Greater than 100

--- skills-sheet/methods_practice.js
+++ skills-sheet/methods_practice.js
@@ -1,6 +1,15 @@
 // 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-var word = window.prompt("Enter a word: ");
-console.log(word.toUpperCase());
+// var word = window.prompt("Enter a word: ");
+// console.log(word.toUpperCase());
 
 //----------------------------------------------------------------------------
+
+// 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// var number = window.prompt("Enter a number: ");
+// if (parseInt(number) > 100) {
+//   console.log("That's a big number");
+// }
+
+//---------------------------------------------------------------------------
