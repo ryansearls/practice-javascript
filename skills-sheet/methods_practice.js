8d34e8e78bc1fb53754a00fbdded13eb16ea20fa
Ryan Searls <Ryan.searls@gmail.com>
2022-07-09 15:45:06 -0600

    All Caps

---
+++ skills-sheet/methods_practice.js
@@ -0,0 +1,6 @@
+// 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+var word = window.prompt("Enter a word: ");
+console.log(word.toUpperCase());
+
+//----------------------------------------------------------------------------
