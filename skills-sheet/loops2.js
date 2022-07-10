0c56e0fdcc3a479e21a937ba0f8ea43e12786314
Ryan Searls <Ryan.searls@gmail.com>
2022-07-10 14:15:08 -0600

    Hello 5 times

--- skills-sheet/loops2.js
+++ skills-sheet/loops2.js
@@ -1,9 +1,19 @@
 // 1. Write a while loop to print the numbers 1 through 10.
 
-var num = 1;
-while (num < 11) {
-  console.log(num);
-  num += 1;
+// var num = 1;
+// while (num < 11) {
+//   console.log(num);
+//   num += 1;
+// }
+
+//-----------------------------------------------------------------------
+
+// 2. Write a while loop that prints the word "hello" 5 times.
+
+var count = 0;
+while (count < 5) {
+  console.log("hello");
+  count += 1;
 }
 
 //-----------------------------------------------------------------------
