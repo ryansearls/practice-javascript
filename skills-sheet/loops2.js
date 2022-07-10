f6960b0c79dafa708253511c26ba71b9830313a6
Ryan Searls <Ryan.searls@gmail.com>
2022-07-10 14:25:50 -0600

    Until stop occurs

--- skills-sheet/loops2.js
+++ skills-sheet/loops2.js
@@ -10,10 +10,21 @@
 
 // 2. Write a while loop that prints the word "hello" 5 times.
 
-var count = 0;
-while (count < 5) {
-  console.log("hello");
-  count += 1;
-}
+// var count = 0;
+// while (count < 5) {
+//   console.log("hello");
+//   count += 1;
+// }
 
 //-----------------------------------------------------------------------
+
+// 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+while (true) {
+  var input = window.prompt("Enter a word: ");
+  if (input === "stop") {
+    break;
+  }
+}
+
+//-------------------------------------------------------------------------
