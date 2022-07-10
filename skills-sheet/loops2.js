2ff39cec1b3b0bc298bf5acd3e0736e1534671d2
Ryan Searls <Ryan.searls@gmail.com>
2022-07-10 14:47:59 -0600

    More than 5 letters

--- skills-sheet/loops2.js
+++ skills-sheet/loops2.js
@@ -72,8 +72,21 @@
 
 // 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
-var count = 0;
-while (count < 144) {
-  console.log("Around the world");
-  count += 1;
+// var count = 0;
+// while (count < 144) {
+//   console.log("Around the world");
+//   count += 1;
+// }
+
+//--------------------------------------------------------------------------------
+
+// 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+while (true) {
+  var input = window.prompt("Enter a word: ");
+  if (input.length > 5) {
+    break;
+  }
 }
+
+//-----------------------------------------------------------------------------------
