8bf521583ff9b567f1b5da15622cf49a42fcb1c5
Ryan Searls <Ryan.searls@gmail.com>
2022-07-10 14:39:04 -0600

    Number Greater than 10

--- skills-sheet/loops2.js
+++ skills-sheet/loops2.js
@@ -41,10 +41,21 @@
 
 // 5. Write a while loop that prints the number 9000 ten times.
 
-var count = 0;
-while (count < 10) {
-  console.log(9000);
-  count += 1;
-}
+// var count = 0;
+// while (count < 10) {
+//   console.log(9000);
+//   count += 1;
+// }
 
 //---------------------------------------------------------------------------
+
+// 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// while (true) {
+//   var input = window.prompt("Enter a number: ");
+//   if (parseInt(input) > 10) {
+//     break;
+//   }
+// }
+
+//-----------------------------------------------------------------------------
