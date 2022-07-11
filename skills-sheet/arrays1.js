672ed189ab6acdb8bc24a7a0458621d737098cda
Ryan Searls <Ryan.searls@gmail.com>
2022-07-11 10:38:17 -0600

    Adds two new instruments

---
+++ skills-sheet/arrays1.js
@@ -0,0 +1,8 @@
+// 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+var words = ["guitar", "piano", "flute"];
+words.push("trumpet");
+words.push("drums");
+console.log(words);
+
+//-------------------------------------------------------------------------------
