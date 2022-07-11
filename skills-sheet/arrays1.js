e4f933f21d2f66315d38137e552ef812cf4304f2
Ryan Searls <Ryan.searls@gmail.com>
2022-07-11 10:51:48 -0600

    5 Numbers

--- skills-sheet/arrays1.js
+++ skills-sheet/arrays1.js
@@ -1,8 +1,27 @@
 // 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-var words = ["guitar", "piano", "flute"];
-words.push("trumpet");
-words.push("drums");
-console.log(words);
+// var words = ["guitar", "piano", "flute"];
+// words.push("trumpet");
+// words.push("drums");
+// console.log(words);
 
 //-------------------------------------------------------------------------------
+
+// 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+// var letters = ["A", "B", "C", "D"];
+// letters[1] = "7";
+// console.log(letters);
+
+//-------------------------------------------------------------------------------
+
+// 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+var numbers = [1,2,3,4,5];
+index = 0;
+while (index < numbers.length) {
+  console.log(numbers[index]);
+  index += 1;
+}
+
+//---------------------------------------------------------------------------------
