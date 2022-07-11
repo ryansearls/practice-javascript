9c176099fd77500a4833e9d3082700cae38a37ab
Ryan Searls <Ryan.searls@gmail.com>
2022-07-11 11:18:47 -0600

    Two element array

--- skills-sheet/arrays1.js
+++ skills-sheet/arrays1.js
@@ -65,8 +65,19 @@
 
 // 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
-var numbers = [3, 4, 24, 3, 1];
-numbers[0] = numbers[0] * 10;
-console.log(numbers);
+// var numbers = [3, 4, 24, 3, 1];
+// numbers[0] = numbers[0] * 10;
+// console.log(numbers);
 
 //-----------------------------------------------------------------------------------------
+
+// 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+var numbers = [93, 23];
+var index = 0;
+while (index < numbers.length) {
+  console.log(numbers[index]);
+  index += 1;
+}
+
+//------------------------------------------------------------------------------------------
