71816471e060e75cbfad5212741713b33ec1b568
Ryan Searls <Ryan.searls@gmail.com>
2022-07-12 10:14:34 -0600

    Adds menu item

--- skills-sheet/hash_practice.js
+++ skills-sheet/hash_practice.js
@@ -9,11 +9,19 @@
 
 // 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
 
-var people = [
-  { firstName: "Seamus", lastName: "Green" },
-  { firstName: "Reilly", lastName: "Searls" },
-  { firstName: "Mel", lastName: "Searls" },
-];
-console.log(people[0]);
+// var people = [
+//   { firstName: "Seamus", lastName: "Green" },
+//   { firstName: "Reilly", lastName: "Searls" },
+//   { firstName: "Mel", lastName: "Searls" },
+// ];
+// console.log(people[0]);
 
 //-------------------------------------------------------------------------------
+
+// 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+var menu = { sandwich: 4, soda: 2, juice: 3 };
+menu["snickers"] = 1;
+console.log(menu);
+
+//--------------------------------------------------------------------------------
