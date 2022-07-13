c499ca675d2adfed530cdad537a810770df21b63
Ryan Searls <Ryan.searls@gmail.com>
2022-07-13 11:38:13 -0600

    first letter of string

--- skills-sheet/functions.js
+++ skills-sheet/functions.js
@@ -27,9 +27,19 @@
 //-----------------------------------------------------------------------------------------------
 
 // 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
-function square(number) {
-  return number * number;
-}
-console.log(square(7));
+
+// function square(number) {
+//   return number * number;
+// }
+// console.log(square(7));
 
 //------------------------------------------------------------------------------------------------
+
+// 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+function firstLetter(string) {
+  return string[0];
+}
+console.log(firstLetter('banana'));
+
+//-----------------------------------------------------------------------------------------------
