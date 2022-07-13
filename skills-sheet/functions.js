f638656d67786c188c0391cb2984c39f4d838d87
Ryan Searls <Ryan.searls@gmail.com>
2022-07-13 11:26:02 -0600

    Upper case function

--- skills-sheet/functions.js
+++ skills-sheet/functions.js
@@ -1,7 +1,18 @@
 // 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
-function doubleNum(number) {
-  return number * 2;
-}
+// function doubleNum(number) {
+//   return number * 2;
+// }
 
-console.log(doubleNum(8));
+// console.log(doubleNum(8));
+
+//---------------------------------------------------------------------------------------------
+
+// 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+function upper(string) {
+  return string.toUpperCase();
+}
+console.log(upper("hello"));
+
+//-----------------------------------------------------------------------------------------------
