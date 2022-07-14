10d9b4dd2f2ee442a94aba02ac4c85d8a6f98584
Ryan Searls <Ryan.searls@gmail.com>
2022-07-14 12:13:58 -0600

    People names in new array

--- skills-sheet/arrays_map.js
+++ skills-sheet/arrays_map.js
@@ -13,9 +13,26 @@
 // 2. Start with an array of strings and create a new array with each string upcased.
 //    For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-var strings = ["hello", "goodbye"];
-var newStrings = [];
-strings.forEach(function (string) {
-  newStrings.push(string.toUpperCase());
+// var strings = ["hello", "goodbye"];
+// var newStrings = [];
+// strings.forEach(function (string) {
+//   newStrings.push(string.toUpperCase());
+// });
+// console.log(newStrings);
+
+//-----------------------------------------------------------------------------------------
+
+// 3. Start with an array of hashes and create a new array of string values from each hash's :name key.
+//    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+var people = [
+  { name: "Alice", age: 27 },
+  { name: "Blane", age: 16 },
+];
+var newPeople = [];
+people.forEach (function (person) {
+  newPeople.push(person.name);
 });
-console.log(newStrings);
+console.log(newPeople);
+
+//--------------------------------------------------------------------------------------------
