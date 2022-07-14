1180918c9b114b444de13f7648d96d58cf3709a1
Ryan Searls <Ryan.searls@gmail.com>
2022-07-14 12:17:32 -0600

    Number plus 7 in new array

--- skills-sheet/arrays_map.js
+++ skills-sheet/arrays_map.js
@@ -25,14 +25,24 @@
 // 3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 //    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
-var people = [
-  { name: "Alice", age: 27 },
-  { name: "Blane", age: 16 },
-];
-var newPeople = [];
-people.forEach (function (person) {
-  newPeople.push(person.name);
-});
-console.log(newPeople);
+// var people = [
+//   { name: "Alice", age: 27 },
+//   { name: "Blane", age: 16 },
+// ];
+// var newPeople = [];
+// people.forEach (function (person) {
+//   newPeople.push(person.name);
+// });
+// console.log(newPeople);
 
 //--------------------------------------------------------------------------------------------
+
+// 4. Start with an array of numbers and create a new array with each number plus 7.
+//    For example, [1, 2, 3] becomes [8, 9, 10].
+
+var numbers = [1, 2, 3];
+var newNumbers = [];
+numbers.forEach(function (number) {
+  newNumbers.push(number + 7);
+});
+console.log(newNumbers);
