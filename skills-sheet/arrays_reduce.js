44a6e004d7b183c2e2ea28bd698a3441847ddf8f
Ryan Searls <Ryan.searls@gmail.com>
2022-07-18 12:12:03 -0600

    minimum number

--- skills-sheet/arrays_reduce.js
+++ skills-sheet/arrays_reduce.js
@@ -25,15 +25,29 @@
 //  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 //     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
-var items = [
-  { name: "chair", price: 100 },
-  { name: "pencil", price: 1 },
-  { name: "book", price: 4 },
-];
-var total = 0;
-items.forEach(function (item) {
-  total += item.price;
-});
-console.log(total);
+// var items = [
+//   { name: "chair", price: 100 },
+//   { name: "pencil", price: 1 },
+//   { name: "book", price: 4 },
+// ];
+// var total = 0;
+// items.forEach(function (item) {
+//   total += item.price;
+// });
+// console.log(total);
 
 //---------------------------------------------------------------------------------------
+
+//  4. Start with an array of numbers and compute the the minumum number.
+//     For example, [5, 10, 8, 3] becomes 3.
+
+// var numbers = [5, 10, 8, 3];
+// var min = numbers[0];
+// numbers.forEach(function (number) {
+//   if (number < min) {
+//     min = number;
+//   }
+// });
+// console.log(min);
+
+//-------------------------------------------------------------------------------------------
