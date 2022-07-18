6a582a57e147390fe27bf198238885e6cbda71a6
Ryan Searls <Ryan.searls@gmail.com>
2022-07-18 11:54:39 -0600

    combine all sports

--- skills-sheet/arrays_reduce.js
+++ skills-sheet/arrays_reduce.js
@@ -1,11 +1,24 @@
 //  1. Start with an array of numbers and compute the sum of all the numbers.
 //     For example, [5, 10, 8, 3] becomes 26.
 
-var numbers = [5, 10, 8, 3];
-var total = 0;
-numbers.forEach(function (number) {
-  total += number;
-});
-console.log(total);
+// var numbers = [5, 10, 8, 3];
+// var total = 0;
+// numbers.forEach(function (number) {
+//   total += number;
+// });
+// console.log(total);
 
 //------------------------------------------------------------------------------
+
+//  2. Start with an array of strings and combine them all into a single string.
+//     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+// var sports = ["volleyball", "basketball", "badminton"];
+// var combined = '';
+// sports.forEach(function (sport) {
+//   combined += sport;
+// });
+// console.log(combined);
+
+//----------------------------------------------------------------------------------
+
