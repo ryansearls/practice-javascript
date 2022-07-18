f4098ac151564127b0a039af71bca915519549b2
Ryan Searls <Ryan.searls@gmail.com>
2022-07-18 12:13:35 -0600

    total words length

--- skills-sheet/arrays_reduce.js
+++ skills-sheet/arrays_reduce.js
@@ -51,3 +51,15 @@
 // console.log(min);
 
 //-------------------------------------------------------------------------------------------
+
+//  5. Start with an array of strings and compute the total length of all the strings.
+//     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+var words = ["volleyball", "basketball", "badminton"];
+var total = 0;
+words.forEach(function (word) {
+  total += word.length;
+})
+console.log(total);
+
+//----------------------------------------------------------------------------------------------
