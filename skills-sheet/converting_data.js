12da7b30ccf1e0a9136f7562393cff5b4892aae4
Ryan Searls <Ryan.searls@gmail.com>
2022-07-19 13:38:32 -0600

    Letter frequency

--- skills-sheet/converting_data.js
+++ skills-sheet/converting_data.js
@@ -33,4 +33,22 @@
 // }
 // console.log(hash);
 
-//--------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+//  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+//     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+
+var word = "bookkeeper";
+var letterFrequencies = {};
+var index = 0;
+while (index < word.length) {
+  var letter = word[index];
+  if (letterFrequencies[letter] === undefined) {
+    letterFrequencies[letter] = 0;
+  }
+  letterFrequencies[letter] += 1;
+  index += 1;
+}
+console.log(letterFrequencies);
+
+//-----------------------------------------------------------------------------------------
