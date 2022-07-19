edf7d5bb142ee34df4be0036008028e5a8f5b936
Ryan Searls <Ryan.searls@gmail.com>
2022-07-19 16:30:38 -0600

    Hash to array of arrays

--- skills-sheet/converting_data.js
+++ skills-sheet/converting_data.js
@@ -38,17 +38,29 @@
 //  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 //     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
 
-var word = "bookkeeper";
-var letterFrequencies = {};
-var index = 0;
-while (index < word.length) {
-  var letter = word[index];
-  if (letterFrequencies[letter] === undefined) {
-    letterFrequencies[letter] = 0;
-  }
-  letterFrequencies[letter] += 1;
-  index += 1;
-}
-console.log(letterFrequencies);
+// var word = "bookkeeper";
+// var letterFrequencies = {};
+// var index = 0;
+// while (index < word.length) {
+//   var letter = word[index];
+//   if (letterFrequencies[letter] === undefined) {
+//     letterFrequencies[letter] = 0;
+//   }
+//   letterFrequencies[letter] += 1;
+//   index += 1;
+// }
+// console.log(letterFrequencies);
 
 //-----------------------------------------------------------------------------------------
+
+//  4. Convert a hash into an array of arrays.
+//     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+
+// var things = { chair: 100, book: 14 };
+// var namePricePairs = [];
+// Object.entries(things).forEach(function ([name, price]) {
+//   namePricePairs.push(name, price);
+// });
+// console.log(namePricePairs);
+
+//----------------------------------------------------------------------------------------
