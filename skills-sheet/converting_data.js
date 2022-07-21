42ad7056f05a207fd74b36166f8b6e1246ba2b06
Ryan Searls <Ryan.searls@gmail.com>
2022-07-20 19:25:53 -0600

    hash to new hash

--- skills-sheet/converting_data.js
+++ skills-sheet/converting_data.js
@@ -64,3 +64,16 @@
 // console.log(namePricePairs);
 
 //----------------------------------------------------------------------------------------
+
+//  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
+//     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+
+var people = { 321: { name: "Alice", age: 31 }, 322: { name: "Maria", age: 27 } };
+var peopleArray = [];
+Object.entries(people).forEach(function ([id, person]) {
+  person.id = parseInt(id);
+  peopleArray.push(person);
+});
+console.log(peopleArray);
+
+//---------------------------------------------------------------------------------------
