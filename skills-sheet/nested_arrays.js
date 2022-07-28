c51e062e07a52b915b29d1c583f951098ba36b85
Ryan Searls <Ryan.searls@gmail.com>
2022-07-28 15:51:16 -0600

    two numbers add up to 10

--- skills-sheet/nested_arrays.js
+++ skills-sheet/nested_arrays.js
@@ -152,23 +152,48 @@
 //  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 //     For example, [1, 8, 3, 10] becomes 18.
 
-var numbers = [1, 8, 3, 10];
-var max = numbers[0] + numbers[1];
-var index1 = 0;
-while (index1 < numbers.length) {
-  var index2 = 0;
-  while (index2 < numbers.length) {
-    if (index1 !== index2) {
-      var sum = numbers[index1] + numbers[index2];
-      if (sum > max) {
-        max = sum;
-      }
-    }
-    index2 ++;
-  }
-  index1 ++;
-}
-console.log(max);
+// var numbers = [1, 8, 3, 10];
+// var max = numbers[0] + numbers[1];
+// var index1 = 0;
+// while (index1 < numbers.length) {
+//   var index2 = 0;
+//   while (index2 < numbers.length) {
+//     if (index1 !== index2) {
+//       var sum = numbers[index1] + numbers[index2];
+//       if (sum > max) {
+//         max = sum;
+//       }
+//     }
+//     index2 ++;
+//   }
+//   index1 ++;
+// }
+// console.log(max);
 
 //----------------------------------------------------------------------------
+
+//  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+
+//For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+
+// var numbers = [2, 5, 3, 1, 0, 7, 11];
+// var result = false;
+// var index1 = 0;
+// while (index1 < numbers.length) {
+//   var currentNumber = numbers[index1];
+//   var index2 = 0;
+//   while (index2 < numbers.length) {
+//     if (index1 !== index2) {
+//       var otherNumber = numbers[index2];
+//       if (currentNumber + otherNumber == 10 && result === false) {
+//         result = [currentNumber, otherNumber];
+//       }
+//     }
+//     index2 += 1;
+//   }
+//   index1 += 1;
+// }
+// console.log(result);
+
+//-------------------------------------------------------------------------------
 
