fc8885aa76b69ee4f94b4d7eea18c3ae4ae63cb5
Ryan Searls <Ryan.searls@gmail.com>
2023-03-14 20:25:05 -0600

    Algo ladder problems

---
+++ algo-ladder/basic-array.js
@@ -0,0 +1,17 @@
+// Write a function that returns the sum of all numbers in a given array.
+
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+// function sumArray(arr) {
+//   let result = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     result += arr[i];
+//   }
+//   return result;
+// }
+
+// console.log(sumArray([1,2,3,4]));
+
+// #==========================================================================
+
