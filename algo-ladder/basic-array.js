5f53de8f1e28c5c59084734ade9a952384109ca8
Ryan Searls <Ryan.searls@gmail.com>
2023-03-14 20:32:35 -0600

    small numbers

--- algo-ladder/basic-array.js
+++ algo-ladder/basic-array.js
@@ -1,4 +1,4 @@
-// Write a function that returns the sum of all numbers in a given array.
+//1. Write a function that returns the sum of all numbers in a given array.
 
 // Input: [1, 2, 3, 4]
 // Output: 10
@@ -15,3 +15,22 @@
 
 // #==========================================================================
 
+// 2. Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+// Input: [99, 101, 88, 4, 2000, 50]
+// Output: [99, 88, 4, 50]
+
+// function smallNumbers(array) {
+//   var smallArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] < 100) {
+//       smallArray.push(array[i]);
+//     }
+//   }
+//   return smallArray;
+// }
+
+
+// console.log(smallNumbers([99, 101, 88, 4, 2000, 50]));
+
+// #===========================================================================
